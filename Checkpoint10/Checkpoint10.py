3bec264ecb9b2268af6b045d3d2e4bba48ce8fd8
MrOlos <86615409+MrOlos@users.noreply.github.com>
2021-07-19 16:12:03 +0100

    Add files via upload

---
+++ Checkpoint10/Checkpoint10.py
@@ -0,0 +1,28 @@
+#index error
+mylist=[14, "hello", 967]
+mylist[2]
+#syntax error
+print("python errors")
+#Key error
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]
+#indentation error
+i=14
+while i<78:
+  print(i)
+  i+=1
+#StopIteration
+it=iter([1,2,3])
+next(it)
+next(it)
+next(it)
+#Type error
+15+15
+#Value error
+int('788')
+#Name error
+python=1
+python
+#ZeroDivisionError
+x=0/19
+x
